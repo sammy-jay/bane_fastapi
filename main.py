6feb5f3baf5fcf102e0b86f8f75fb9d8707107ad
sammy-jay <sammyjayden27@gmail.com>
2023-08-12 16:03:13 +0100

    minor additions

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
-from app.routers import posts
+from app.routers import posts, users, auth
 
 app = FastAPI()
+
+app.include_router(auth.router)
+app.include_router(users.router)
 app.include_router(posts.router)
 
 @app.get("/")
